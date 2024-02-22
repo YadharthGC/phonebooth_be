b752d677226a5052cea5f60f8083db19b20fb712
YadharthGC <ganeshyadharth@gmail.com>
2024-02-22 13:17:49 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const cors = require("cors");
-const port = 3009;
+const port = process.env.PORT || 3009;
 const routez = require("./routes/boothRoute");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "50mb" }));
